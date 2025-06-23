a9c5c91230f553a87d5f81fc78f0a4213825febe
Zaheer2003 <mhdzaheer2003@gmail.com>
2025-06-23 21:49:33 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,13 @@
-import About from "@/components/About";
-import Certificates from "@/components/Certificates";
-import Contact from "@/components/Contact";
-import Education from "@/components/Education";
+
 import Hero from "@/components/Hero";
-import Projects from "@/components/Projects";
-import Skills from "@/components/Skills";
+
 
 
 export default function Home() {
   return (
     <>
       <Hero/>
-      <About/>
-      <Education/>
-      <Skills/>
-      <Certificates/>
-      <Projects/>
-      <Contact/>
+
     </>
     
   );
